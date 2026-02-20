d38d7e4af6187343694440c011b5b7ecf021013f
KOVALW <AD71@cdc.gov>
2026-02-20 17:57:22 +0000

    finish porting unit tests

--- src/calibrationtools/spawn_rng.py
+++ src/calibrationtools/spawn_rng.py
@@ -1,5 +1,6 @@
 from numpy.random import Generator, SeedSequence, default_rng
 
+
 def spawn_rng(seed_sequence: SeedSequence) -> Generator:
     """Spawn a new RNG from the given seed sequence."""
     return default_rng(seed_sequence.spawn(1)[0])
